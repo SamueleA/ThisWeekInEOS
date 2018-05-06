2b20590bcf7b9ceefefea014f0692fad79fbc71d
Samuele Agostinelli <batonenbois@gmail.com>
2018-05-06 19:43:22 -0400

    minor change

--- client/server.js
+++ client/server.js
@@ -9,6 +9,6 @@ app.get('*',(req, res)=>{
   res.sendFile(path.resolve(__dirname, 'dist/index.html'));
 });
 
-app.listen(port, ()=>{
+app.listen(port, '0.0.0.0', ()=>{
   console.log(`Server started on ${port}`)
 });
